2c4d272feb4227b0ea79c4e1cccbaf019bc82028
Brayan_Giraldo <giraldo8900@gmail.com>
2024-10-15 08:03:08 -0500

    form

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function iniciarSesion(){
     console.log('Inicio de sesión')
+    let label = document.getElementById('label')
+    console.log(label)
+
 }
+
+
+console.log(document.getElementById('label'))
+console.log(document.getElementById('label').textContent = 'Esto es un nuevo valor')
+
